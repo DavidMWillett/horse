00b9c1b8c408f15427499cc93918d7775f89f991
david <david.m.willett@gmail.com>
2021-03-05 16:51:36 +0000

    Initial commit

---
+++ src/test/kotlin/com/dajati/horse/HorseApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dajati.horse
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HorseApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
