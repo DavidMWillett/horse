c0f2ad67398de32eaea3d1a72d5b3b5d2cb8c57c
david <david.m.willett@gmail.com>
2021-03-06 17:37:52 +0000

    All classes now correctly deserialized.

--- src/main/kotlin/com/dajati/horse/domain.kt
+++ src/main/kotlin/com/dajati/horse/domain.kt
@@ -2,13 +2,35 @@ package com.dajati.horse
 
 class Roster(val employeeList: List<Employee>, val taskList: List<Task>)
 
-class Employee(val name: String, val team: Team)
+class Task(val duty: Duty, val shift: Shift, val employee: Employee?)
+
+class Employee(val name: String, val team: Team, val availability: Availability)
 
 enum class Team {
     PRINCIPALS,
     AML_MDS,
     MPN_CML,
-    LYMPHOID;
+    LYMPHOID,
 }
 
-class Task
+class Availability(val entries: List<List<Boolean>>)
+
+enum class Duty {
+    FISH,
+    DS,
+    LATE_DS,
+    SS,
+}
+
+enum class Shift {
+    MONDAY_AM,
+    MONDAY_PM,
+    TUESDAY_AM,
+    TUESDAY_PM,
+    WEDNESDAY_AM,
+    WEDNESDAY_PM,
+    THURSDAY_AM,
+    THURSDAY_PM,
+    FRIDAY_AM,
+    FRIDAY_PM,
+}
