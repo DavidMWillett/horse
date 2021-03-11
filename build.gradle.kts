f6e5691fd9bcca5a86d486f2f6b27cb97cf86087
david <david.m.willett@gmail.com>
2021-03-11 17:17:27 +0000

    Added build tools to build and install web client. Version 0.3.0.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,16 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.moowork.gradle.node.npm.NpmTask
 
 plugins {
 	id("org.springframework.boot") version "2.4.3"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	id("com.moowork.node") version "1.3.1"
 	kotlin("jvm") version "1.4.30"
 	kotlin("plugin.spring") version "1.4.30"
 }
 
 group = "com.dajati"
-version = "0.0.1-SNAPSHOT"
+version = "0.3.0"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
@@ -33,4 +35,37 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+node {
+	download = false
+
+	// Set the work directory for unpacking node
+	workDir = file("${project.buildDir}/nodejs")
+
+	// Set the work directory for NPM
+	npmWorkDir = file("${project.buildDir}/npm")
+}
+
+tasks.register<NpmTask>("appNpmInstall") {
+	description = "Installs all dependencies from package.json"
+	setWorkingDir(file("${project.projectDir}/src/main/webapp"))
+	setArgs(listOf("install"))
+}
+
+tasks.register<NpmTask>("appNpmBuild") {
+	dependsOn("appNpmInstall")
+	description = "Builds production version of the webapp"
+	setWorkingDir(file("${project.projectDir}/src/main/webapp"))
+	setArgs(listOf("run", "build"))
+}
+
+tasks.register<Copy>("copyWebApp") {
+	dependsOn("appNpmBuild")
+	from("src/main/webapp/build")
+	into("build/resources/main/static/.")
+}
+
+tasks.named("build") {
+	dependsOn("copyWebApp")
 }
